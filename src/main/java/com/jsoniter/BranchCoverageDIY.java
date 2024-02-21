42e6c200a93240b17ea7ae0077a46005e4513910
lvainio <lvainio@kth.se>
2024-02-21 09:43:00 +0100

    Implemented BranchCoverageDIY class

--- src/main/java/com/jsoniter/BranchCoverageDIY.java
+++ src/main/java/com/jsoniter/BranchCoverageDIY.java
@@ -2,7 +2,18 @@ package com.jsoniter;
 
 public class BranchCoverageDIY {
     
-    
+    private static final int NUM_BRANCHES = 10; // TODO: CHANGE TO ACTUAL COUNT
+    private static boolean[] branchesReached = new boolean[NUM_BRANCHES];
 
+    public static boolean[] getBranchesReached() {
+        return branchesReached;
+    }
 
+    public static void setBranchReached(int branchID) {
+        if (branchID >= 0 && branchID < NUM_BRANCHES) {
+            branchesReached[branchID] = true;
+        } else {
+            throw new IllegalArgumentException("Invalid branch ID");
+        }
+    }
 }
